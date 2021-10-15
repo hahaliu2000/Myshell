c53ffb34535a0f4a2c91b3b48830c70e466724ef
hahaliu2000 <liuxzh2000@gmail.com>
2021-10-14 23:03:40 -0400

    Initial Commit

---
+++ Myshell/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Myshell
+//
+//  Created by Xuanzhang on 10/14/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
